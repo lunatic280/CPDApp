9f81269b515baccfde163c7ed84eaae911785414
홍지민 <82097844+lunatic280@users.noreply.github.com>
2025-03-21 00:31:18 +0900

    네비게이션 예제 연습

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/example/cpdandroid/MainActivity.kt
+++ app/src/main/java/com/example/cpdandroid/MainActivity.kt
@@ -13,6 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.cpdandroid.ui.theme.CPDAndroidTheme
 import com.example.app.network.RetrofitClient
 import kotlinx.coroutines.launch
@@ -23,14 +28,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CPDAndroidTheme {
-                MainScreen()
+                NavigationApp()
             }
         }
     }
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavController) {
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
     var message by remember { mutableStateOf("Press the button") }
@@ -55,5 +60,81 @@ fun MainScreen() {
         }) {
             Text("Fetch Data")
         }
+
+        //뒤로 가는 버튼
+        Button(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Back to Home")
+        }
     }
+}
+
+@Composable
+fun NavigationApp() {
+
+    //NavController 생성 (화면 전환 관리)
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController)} //홈화면 정하기
+        composable("details") { DetailScreen(navController)}
+        composable("get") { MainScreen(navController)}
+    }
+}
+
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { navController.navigate("details") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Go to Details")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { navController.navigate("get") },
+            modifier =  Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Fetch API Data")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is the Details Screen", modifier = Modifier.padding(16.dp))
+
+        Button(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Back to Home")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NavigationApp()
 }
